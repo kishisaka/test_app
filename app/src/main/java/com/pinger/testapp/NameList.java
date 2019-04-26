8e370e4289da155ff00d276d5aa1e47b3853d02d
Kurt Ishisaka <>
2019-04-26 10:55:31 -0700

    add linear layout manager

--- app/src/main/java/com/pinger/testapp/NameList.java
+++ app/src/main/java/com/pinger/testapp/NameList.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -51,6 +52,7 @@ public class NameList extends AppCompatActivity {
         nameList = getNameList();
         nameAdapter = new NameAdapter(nameList);
         recyclerView = (RecyclerView)findViewById(R.id.name_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(nameAdapter);
     }
 
